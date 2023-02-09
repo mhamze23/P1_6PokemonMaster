55f4b4651063114580d1a6e41c2f75506171a6d8
DomenicoBarb <domenicobarberi585@gmail.com>
2023-02-08 19:22:56 -0500

    fixed working dev branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 $(function () {
 
-  localStorage.clear();
-
   var pokemonInput = $("#pokemon-input");
   var formEl = $("#form");
   var pokeName1 = $("#pokeName1")[0];
@@ -165,11 +163,7 @@ $(function () {
     "Mew": null,
   };
 
-  // $("#dialog").dialog({
-  //   autoOpen: false
-  // })
-
-  // getting stuff from local storage for pokemon
+    // getting stuff from local storage for pokemon
   var pokemonStorage = localStorage.getItem("pokemon");
 
     // create or add to an array
@@ -188,7 +182,7 @@ $(function () {
       text: pokemon
     });
       
-    btn.addClass("btn");
+    btn.addClass("btn ");
     pokemonBtns.prepend(btn);
   }
 
@@ -203,70 +197,48 @@ $(function () {
   // video is an empty string to start, will be generated
   var video = '';
 
-  function invalidAlert() {
-    $("#modal1").modal()
-  }
-
   // submit event listener
   formEl.submit(function (event) {
-    event.preventDefault();
+    event.preventDefault()
+    var search = pokemonInput.val().split(" ")[0].trim().toLowerCase()
 
-    if (pokemonInput.val() === "" || pokemonInput.val() === null) {
-      invalidAlert();
-      pokemonInput.val("")
-    } else {
-      var search = pokemonInput.val().split(" ")[0].trim().toLowerCase()
-      gen1Pokemon(search);
-      pokemonInput.val("")
-    }
-  });
+    // for debugging purposes, confirms a form is submitted
+    //alert("form is submitted")//
 
-  function gen1Pokemon(search) {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then((response) => response.json())
-      .then((data) => {
-        nameArray = [];
-        for (var i = 0; i < 151; i++) {
-          nameArray[i] = data.results[i].name;
-        }
+    // this will be altered to our counter pokemon name when that var is created
+    // still need poke api logic to get that variable/value
+    // currently links to the inputted pokemon name, not counter pokemon name like the final product
 
-        if (nameArray.includes(search) === true) {
-          getPokemon(search);
-        } else {
-          invalidAlert();
-        }
-    })
-  }
+    // videoSearch config
+    // videoSearch(apiKey, search, 1)
 
-  
-  function getPokemon(search) {
+    function getPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon/' + search)
       .then((response) => response.json())
       .then((data) => {
 
-      var pokemonName1 = data.name;
-      var pokemonType1 = data.types[0].type.name;
-      
+        var pokemonName1 = data.name;
+        var pokemonType1 = data.types[0].type.name;
+        
         var pokemonName1Text = pokemonName1.toUpperCase();
         var pokemonType1Text = "Type: " + pokemonType1.toUpperCase();
         $("#pokeImage1")[0].src = "./assets/images/" + pokemonType1 + "/" + pokemonName1 + ".png";
-  
+
         if (pokemonName1Text && pokemonStorage.includes(pokemonName1Text) === false) {
           createBtn(pokemonName1Text);
           pokemonStorage.push(pokemonName1Text);
           localStorage.setItem("pokemon", JSON.stringify(pokemonStorage));
+        }
 
-  
         pokeName1.innerHTML = pokemonName1Text;
         pokeType1.innerHTML = pokemonType1Text
-  
-        findWeakness(pokemonType1);
-      } else {
-        invalidAlert();
-      }
-    });
-  };
 
+        findWeakness(pokemonType1);
+      });
+    };
+    getPokemon();
+  });
+  
   function findWeakness(pokemonType1) {
     fetch('https://pokeapi.co/api/v2/type/' + pokemonType1)
     .then((response) => response.json())
@@ -565,7 +537,6 @@ $(function () {
     list.empty();
   }
 
-
   // click event to search the same city again if button are created already
   history.on('click', searchAgain)
 
@@ -594,4 +565,3 @@ $(function () {
     })
   }
 });
-
